b58e029420d4361f39a0dd355594ae5272bc1af2
Ali <rehatski@gmail.com>
2018-12-16 12:40:38 -0800

    added vscode settings and added to plain install script

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ ln -s ${BASEDIR}/.inputrc ~/.inputrc
 ln -s ${BASEDIR}/.tern-project ~/.tern-project
 ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
 
+# Install VSCODE first
+ln -sf ${BASEDIR}/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ${BASEDIR}/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
 ln -s ${BASEDIR}/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
